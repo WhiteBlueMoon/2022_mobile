f1c68be383d2a79cba53b5d1957a152beb2d4dac
chensoyul <115689021+chensoyul@users.noreply.github.com>
2022-11-03 01:06:48 +0900

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.teamproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button gamestart ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gamestart = (Button) findViewById(R.id.game_start);
+
+        gamestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), main.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
